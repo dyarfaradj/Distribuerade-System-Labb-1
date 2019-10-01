af052901adcc2650acfa66e9d2bb2e9baa6ac44b
Kamalnadhmi <49027730+Kamalnadhmi@users.noreply.github.com>
2019-10-01 14:58:07 +0200

    Update ItemDAO.java

--- src/DataAccess/ItemDAO.java
+++ src/DataAccess/ItemDAO.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import Business.String;
+import Business.Items;
 import net.javaguides.usermanagement.model.Items;
 
 /**
