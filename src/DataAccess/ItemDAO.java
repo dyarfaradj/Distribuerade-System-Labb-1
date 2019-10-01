375000c36ed1cf44c562aae88b00c671c7dc3aa0
Kamalnadhmi <49027730+Kamalnadhmi@users.noreply.github.com>
2019-10-01 15:01:02 +0200

    Update ItemDAO.java

--- src/DataAccess/ItemDAO.java
+++ src/DataAccess/ItemDAO.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import Business.Items;
+import Business.Item;
 
 /**
  * AbstractDAO.java This DAO class provides CRUD database operations for the
