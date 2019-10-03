f0a14381145ef0b23e3ed953721673e68b2933c7
iPwned <dyar.98@hotmail.com>
2019-10-04 01:04:47 +0200

    Update CartServlet.java

--- src/Business/CartServlet.java
+++ src/Business/CartServlet.java
@@ -1,86 +1,88 @@
 package Business;
 
-
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.List;
-import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.RequestDispatcher;
 
 import DataAccess.CartDao;
 
-@WebServlet(name="CartServlet", urlPatterns={"/showcart", "/addtocart"})
+@WebServlet(name = "CartServlet", urlPatterns = { "/showcart", "/addtocart" })
 public class CartServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
 	private CartDao cartDAO;
 
-    public void init() {
-    	cartDAO = new CartDao();
-    }
-				
-    public CartServlet() {
-        super();
-    }
+	public void init() {
+		cartDAO = new CartDao();
+	}
 
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    	    throws ServletException, IOException {
-    	        String action = request.getServletPath();
-    	        try {
-    	            switch (action) {
-    	                case "/showcart":
-    	                	showCart(request, response);
-    	                    break;
-    	                case "/addtocart":
-    	                	addToCart(request, response);
-    	                    break;
-    	                default:
-    	                    break;
-    	            }
-    	        } catch (SQLException ex) {
-    	            throw new ServletException(ex);
-    	        }
-    	    }
+	public CartServlet() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String action = request.getServletPath();
+		try {
+			switch (action) {
+			case "/showcart":
+				showCart(request, response);
+				break;
+			case "/addtocart":
+				addToCart(request, response);
+				break;
+			default:
+				break;
+			}
+		} catch (SQLException ex) {
+			throw new ServletException(ex);
+		}
+	}
+
 	private void addToCart(HttpServletRequest request, HttpServletResponse response)
-		    throws SQLException, IOException, ServletException {
-		int user_id= (int) request.getSession().getAttribute("user_id");
-		int product_id= Integer.parseInt(request.getParameter("product_id"));
-		List < Cart > listItem = cartDAO.selectAllItems(user_id);
-		for() {
-			
+			throws SQLException, IOException, ServletException {
+		int user_id = (int) request.getSession().getAttribute("user_id");
+		int product_id = Integer.parseInt(request.getParameter("product_id"));
+		int quantity = 0;
+		List<Cart> listItem = cartDAO.selectAllItems(user_id);
+		for (int i = 0; i < listItem.size(); i++) {
+			if (listItem.get(i).getProduct_id() == product_id) {
+				quantity = listItem.get(i).getQuantity();
+			}
 		}
-		int quantity = 1);
-		
 		int stockId = CartDao.getStock(product_id);
-		
-		if(stockId >=quantity) {
-			CartDao.insertIntoCart(user_id,product_id,quantity);	
+
+		if (stockId >= quantity) {
+			CartDao.insertIntoCart(user_id, product_id, quantity);
 		}
-		
+
 		showCart(request, response);
 	}
-	
-	private void showCart(HttpServletRequest request, HttpServletResponse response)
-		    throws SQLException, IOException, ServletException {
-		int user_id= (int) request.getSession().getAttribute("user_id");
-		
-		List < Cart > listItem = cartDAO.selectAllItems(user_id);
-        request.setAttribute("listItem", listItem);
-        RequestDispatcher dispatcher = request.getRequestDispatcher("/cart.jsp");
-        dispatcher.forward(request, response);
-     }
 
+	private void showCart(HttpServletRequest request, HttpServletResponse response)
+			throws SQLException, IOException, ServletException {
+		int user_id = (int) request.getSession().getAttribute("user_id");
+
+		List<Cart> listItem = cartDAO.selectAllItems(user_id);
+		request.setAttribute("listItem", listItem);
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/cart.jsp");
+		dispatcher.forward(request, response);
+	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 	}
 
 }
